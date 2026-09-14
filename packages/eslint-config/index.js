2c903acd93e211129c1d0e13ae9980173b4e3b3c
Jull Weber <jull@monkeykode.com>
2026-09-14 12:41:56 -0700

    feat: add shared typescript-config and eslint-config packages

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,17 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTypeScript from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier'
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTypeScript,
+  prettier,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+])
